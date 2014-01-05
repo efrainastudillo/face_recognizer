0e63e60171686d9e0c743ad4579ff4330bb28869
Efrain Astudillo <efrainastudillo@Efrains-MacBook-Pro.local>
2014-01-05 01:19:05 -0500

    Initial Commit

---
+++ FaceRecognizer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FaceRecognizer
+//
+//  Created by Efrain Astudillo on 1/5/14.
+//  Copyright (c) 2014 Efrain Astudillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
