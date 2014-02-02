55d501034a9d609e92423bc85333150f5fda9039
efrainastudillo <efrain.astudillo58@gmail.com>
2014-02-02 16:36:50 -0500

    Se graba el archivo con los nombres de los usuarios y con su respectiva clase, se inicia la aplicacion cargando en memoria todos los datos grabados previamente en los archivos para relizar el training

--- BaseApp/src/AIUtil.h
+++ BaseApp/src/AIUtil.h
@@ -54,8 +54,9 @@ inline static void EIGEN2STL(){
 #define __IMAGES_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/BaseApp/resources/"
 #define __IMAGES_PATH_TEST__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/FaceRecognizer/FaceRecognizer/imagesTest/"
 #define __HAARCASCADE_PATH__ "/Users/efrainastudillo/Documents/Development/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"
-#define __TRAINING_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/BaseApp/resources/images_training.csv"
-#define __TEST_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/BaseApp/resources/images_test.csv"
+#define __TRAINING_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/FaceRecognizer/BaseApp/resources/images_training.csv"
+#define __TRAINING_NAMES_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/FaceRecognizer/BaseApp/resources/names_training.csv"
+#define __TEST_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/FaceRecognizer/BaseApp/resources/images_test.csv"
 #define __MODEL_PATH__ "/Users/efrainastudillo/Documents/Development/XcodeProjects/FaceRecognizer/FaceRecognizer/trainingModel/"
 
 #elif __PETER__ //Pedro Lucas Path
@@ -65,6 +66,7 @@ inline static void EIGEN2STL(){
 #define __IMAGES_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/images/"
 #define __HAARCASCADE_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/data/haarcascade_frontalface_alt.xml"
 #define __TRAINING_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/testingFiles/images_training.csv"
+#define __TRAINING_NAMES_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/testingFiles/images_names_training.csv"
 #define __TEST_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/testingFiles/images_test.csv"
 #define __MODEL_PATH__ "D:/AIProject/face_recognizer/FaceRecognizer/trainingModel/"
 
--- BaseApp/src/BaseApp.cpp
+++ BaseApp/src/BaseApp.cpp
@@ -99,15 +99,14 @@ void TutorialApp::setup()
     mSaveDataButton.setPosition(ci::Rectf(left + width_rect*0.5,top + height_rect*0.53,width_rect*0.45,40));
     mSaveModelButton.setPosition(ci::Rectf(left + width_rect*0.5,top + height_rect*1.53,width_rect*0.45,40));
     
-    //mCamera = new AICamera(&mMessage);
-    mCamera.initialize();
     mTextInput.initialize();
-    
+    mCamera.mDataSet.loadDataFromFile(__TRAINING_PATH__);
+    mCamera.mDataSet.loadNamesFromFile(__TRAINING_NAMES_PATH__);
+    mCamera.initialize();// camera initilization should be before to load names and data from files
     
     //===============     Artificial Intelligence       ================//
     mLda = AILda();
     mNN = AINearestNeighbor();
-    mDataSet = mCamera.get_dataSet();
     mBuilder = AIBuilder(mLda, mNN);
     
     
@@ -154,11 +153,11 @@ void TutorialApp::mouseDown(cinder::app::MouseEvent event){
     }
     else if(mBuildButton.isEvent())
     {
-        mCamera.mDataSet.loadData();
+        //mCamera.mDataSet.loadData();
         console()<< "Data Size: "<<mCamera.mDataSet.data.size()<<std::endl;
         console()<< "Labels Size: "<<mCamera.mDataSet.labels.size()<<std::endl;
         
-        mBuilder.compute(mCamera.mDataSet.data, mCamera.mDataSet.labels);
+        //mBuilder.compute(mCamera.mDataSet.data, mCamera.mDataSet.labels);
     }
     else if (mPredictButton.isEvent())
     {
@@ -166,7 +165,8 @@ void TutorialApp::mouseDown(cinder::app::MouseEvent event){
     }
     else if (mSaveDataButton.isEvent())
     {
-        mCamera.mDataSet.saveToFile(__TRAINING_PATH__);
+        mCamera.mDataSet.saveDataToFile(__TRAINING_PATH__);
+        mCamera.mDataSet.saveNamesToFile(__TRAINING_NAMES_PATH__);
     }
     else if (mSaveModelButton.isEvent())
     {
